2376cc9bf4c2183b9deed9afe42d441eb65dbae1
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2016-08-16 12:42:56 +0300

    tracker field can be null.

--- db/migrate/001_create_reminder_entries.rb
+++ db/migrate/001_create_reminder_entries.rb
@@ -2,7 +2,7 @@ class CreateReminderEntries < ActiveRecord::Migration
   def change
     create_table :reminder_entries do |t|
       t.references :project, null: false
-      t.references :tracker, null: false, :default => 0
+      t.references :tracker, :default => 0
       t.integer :days, null: false
       t.string :env, null: false
     end
