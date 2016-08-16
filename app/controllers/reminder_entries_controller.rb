25451de001ad4a46982d5641376edc02f341a941
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2016-08-16 12:44:56 +0300

    create, destroy actions defined.

--- app/controllers/reminder_entries_controller.rb
+++ app/controllers/reminder_entries_controller.rb
@@ -8,10 +8,6 @@ class ReminderEntriesController < ApplicationController
     @reminder_entries = ReminderEntry.where(project_id: @project.id)
   end
 
-  def create
-
-  end
-
   def new
     @project = Project.find(params[:project_id])
     @reminder_entry = ReminderEntry.new
@@ -36,22 +32,60 @@ class ReminderEntriesController < ApplicationController
       tracker = Tracker.find_by_id(project_tracker_id)
       @trackeroptions.push([tracker.name, project_tracker_id])
     end
-
   end
 
-  def edit
+  def create
+    @project = Project.find(params[:project_id])
+    if params[:reminder][:tracker] == ""
+      tracker_id = 0
+    else
+      tracker_id = params[:reminder][:tracker]
+    end
+    @reminder_entry = ReminderEntry.new(:project_id => @project.id,
+                                        :tracker_id => tracker_id,
+                                        :days => params[:reminder][:days],
+                                        :env => params[:reminder][:env])
 
-  end
+    if @reminder_entry.save
+      @reminder_users = Array.new
+      user_ids = params[:reminder][:users]
+      user_ids.each do |user_id|
+        unless user_id == ""
+          reminder_user = ReminderUser.new(:reminder_entry_id => @reminder_entry.id,
+                                           :user_id => user_id)
+          @reminder_users.push(reminder_user)
+        end
+      end
 
-  def show
-
-  end
-
-  def update
-
+      @reminder_users.each do |reminder_user|
+        reminder_user.save
+      end
+      flash[:success] = "Reminder set."
+      redirect_to project_reminder_entries_path(:project_id => @project.id)
+    else
+      flash.now[:alert] = "Reminder couldn't set! Please check the form."
+      Rails.logger.info(@reminder_entry.errors.inspect)
+      render new_project_reminder_entry_path(:project_id => @project.id)
+    end
   end
 
   def destroy
+    @project = Project.find(params[:project_id])
+    @reminder_entry = ReminderEntry.find(params[:id])
+    @status = @reminder_entry.destroy
 
+    if @status
+      @reminder_users = ReminderUser.where(reminder_entry_id: @reminder_entry.id)
+      @reminder_users.each do |reminder_user|
+        reminder_user.destroy
+      end
+
+      flash[:success] = "Reminder removed."
+      redirect_to project_reminder_entries_path
+    else
+      flash.now[:alert] = "Reminder couldn't be removed!"
+      render project_reminder_entries_path
+    end
   end
+
 end
