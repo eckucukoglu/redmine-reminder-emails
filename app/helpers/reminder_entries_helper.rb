6296061e67ed2e58037c3d94be6d6ead99a55a5b
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2016-08-16 12:45:49 +0300

    helper implemented to get tracker name. little bug fixed at getReminderUserNames.

--- app/helpers/reminder_entries_helper.rb
+++ app/helpers/reminder_entries_helper.rb
@@ -4,17 +4,36 @@ module ReminderEntriesHelper
     reminder_users = ReminderUser.where(reminder_entry_id: reminder_entry_id)
     if reminder_users != nil
       reminder_users.each do |reminder_user|
-        user = User.find_by_id(reminder_user.id)
-        reminderUserNames.push(user.firstname + " " + user.lastname)
+        user = User.find_by_id(reminder_user.user_id)
+        if user != nil
+          reminderUserNames.push(user.firstname + " " + user.lastname)
+        else
+          reminderUserNames.push("#" + reminder_user.user_id.to_s)
+        end
       end
     end
 
     if reminderUserNames.length == 0
       return "Everyone"
     end
-    
+
     return reminderUserNames
   end
+
+  def getReminderTrackerName(tracker_id)
+    if tracker_id == 0
+      return "All trackers"
+    end
+
+    tracker = Tracker.find_by_id(tracker_id)
+    if tracker != nil
+      return tracker.name
+    else
+      return ("#" + tracker_id.to_s)
+    end
+  end
+
+
 
 
 
